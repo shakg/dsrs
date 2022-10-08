b3849672cce1cfc5fe0c64a3fc53f8fc48021689
ishak Gönül <ishak.gonul@titra.com.tr>
2022-10-08 23:06:52 +0300

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
